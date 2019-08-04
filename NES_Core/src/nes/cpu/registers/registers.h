83cab10fc724bb6efc3e93a7cb817337d1512517
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-08-04 19:36:41 +0200

    Added registers file

---
+++ NES_Core/src/nes/cpu/registers/registers.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <cstdint>
+#include <bitset>
+
+namespace nes::cpu::registers {
+	enum class ProcessorStatus : uint8_t
+	{
+		Carry = 0,
+		Zero = 1,
+		Interrupt = 2,
+		Decimal = 3,
+		Break = 4,
+		Overflow = 5,
+		Negative = 6
+	};
+
+	struct Registers
+	{
+		int8_t A;
+		int8_t X;
+		int8_t Y;
+		uint8_t SP;
+		uint16_t PC;
+		std::bitset<8> PS;
+
+		//TODO sk: verify default values of registers
+		Registers() :
+			A(0),
+			X(0),
+			Y(0),
+			SP(0),
+			PS(0b00110100),
+			PC(0)
+		{
+		}
+	};
+}
+
