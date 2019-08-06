8972083a9e7e7d9ae0474d54f699301d34eeaf4c
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-08-06 23:07:21 +0200

    Helper math methods are conditionally noexcept

--- NES_Core/src/helpers/math.h
+++ NES_Core/src/helpers/math.h
@@ -2,13 +2,13 @@
 
 namespace helpers::math {
 	template<typename T> 
-	inline bool isOverflow(T lhs, T rhs, T result)
+	inline bool isOverflow(T lhs, T rhs, T result) noexcept(noexcept((lhs < 0 && rhs < 0 && result > 0) || (lhs > 0 && rhs > 0 && result < 0)))
 	{
 		return (lhs < 0 && rhs < 0 && result > 0) || (lhs > 0 && rhs > 0 && result < 0);
 	}
 
 	template<typename T>
-	inline bool isCarry(T lhs, T rhs)
+	inline bool isCarry(T lhs, T rhs) noexcept(noexcept((lhs < 0 && rhs > 0 && rhs >= -lhs) || (rhs < 0 && lhs > 0 && lhs >= -rhs) || (lhs < 0 && rhs < 0)))
 	{
 		return (lhs < 0 && rhs > 0 && rhs >= -lhs) || (rhs < 0 && lhs > 0 && lhs >= -rhs) || (lhs < 0 && rhs < 0);
 	}
