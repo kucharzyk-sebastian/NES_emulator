f6e583fed86298dcb1b402d1ee4b8cf8760bd6ef
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-08-10 19:36:39 +0200

    Added more unit tests for immediate AND

--- NES_Core_CPU_Tests/opcodes/immediate/Executor_AND_Tests.cpp
+++ NES_Core_CPU_Tests/opcodes/immediate/Executor_AND_Tests.cpp
@@ -19,12 +19,82 @@ namespace OPCodes_Immediate_Executor
 		{
 		}
 
-		TEST_METHOD(AND_performs_logical_and)
+		TEST_METHOD(AND_performs_binary_and)
 		{
-			ie_.AND(int8_t(3));
-			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
-			ie_.ADC(int8_t(1));
+			ie_.LDA(int8_t(0b00001011));
+
+			ie_.AND(int8_t(0b00100110));
+
+			Assert::AreEqual(reg_.A, int8_t(0b00000010));
+		}
+
+		TEST_METHOD(AND_sets_zero_flag_for_zero_result)
+		{
+			ie_.LDA(int8_t(0b00100010));
 			Assert::IsFalse(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
+
+			ie_.AND(int8_t(0b00001000));
+
+			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
+		}
+
+		TEST_METHOD(AND_resets_zero_flag_for_positive_result)
+		{
+			ie_.LDA(int8_t(0b00001111));
+			auto registers = ie_.getRegisters();
+			registers.PS.set(static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero));
+			ie_.setRegisters(registers);
+			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
+
+			ie_.AND(int8_t(0b00001010));
+
+			Assert::IsFalse(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
+		}
+
+		TEST_METHOD(AND_resets_zero_flag_for_negative_result)
+		{
+			ie_.LDA(int8_t(0b10001111));
+			auto registers = ie_.getRegisters();
+			registers.PS.set(static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero));
+			ie_.setRegisters(registers);
+			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
+
+			ie_.AND(int8_t(0b10001110));
+
+			Assert::IsFalse(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Zero)]);
+		}
+
+		TEST_METHOD(AND_sets_negative_flag_for_negative_results)
+		{
+			ie_.LDA(int8_t(0b10001100));
+			auto registers = ie_.getRegisters();
+			registers.PS.reset(static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative));
+			ie_.setRegisters(registers);
+			Assert::IsFalse(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative)]);
+
+			ie_.AND(int8_t(0b10000000));
+
+			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative)]);
+		}
+
+		TEST_METHOD(AND_resets_negative_flag_for_positive_results)
+		{
+			ie_.LDA(int8_t(0b10011001));
+			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative)]);
+
+			ie_.AND(int8_t(0b00011000));
+
+			Assert::IsFalse(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative)]);
+		}
+
+		TEST_METHOD(AND_resets_negative_flag_for_zero_result)
+		{
+			ie_.LDA(int8_t(0b10011001));
+			Assert::IsTrue(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative)]);
+
+			ie_.AND(int8_t(0b00100100));
+
+			Assert::IsFalse(reg_.PS[static_cast<uint8_t>(nes::cpu::registers::ProcessorStatus::Negative)]);
 		}
 
 	};
