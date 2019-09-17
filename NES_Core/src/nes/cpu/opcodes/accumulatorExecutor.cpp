938c74912d209b8558da3b4b342497cdb29bb50f
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-09-17 19:49:15 +0200

    Implemented accumulator ASL instructionn

---
+++ NES_Core/src/nes/cpu/opcodes/accumulatorExecutor.cpp
@@ -0,0 +1,17 @@
+#include "nes/cpu/opcodes/accumulatorExecutor.h"
+#include "accumulatorExecutor.h"
+
+namespace nes::cpu::opcodes{
+	nes::cpu::opcodes::AccumulatorExecutor::AccumulatorExecutor(nes::cpu::registers::Registers& registers, nes::memory::Memory& memory) :
+	BasicExecutor(registers, memory)
+	{
+	}
+
+	void nes::cpu::opcodes::AccumulatorExecutor::ASL() noexcept
+	{
+		registers_.PS[static_cast<uint8_t>(rps::Carry)] = registers_.A >> (sizeof(unsigned int) * CHAR_BIT - 1);
+		registers_.A = registers_.A << 1;
+		registers_.PS[static_cast<uint8_t>(rps::Negative)] = registers_.A < 0 ? true : false;
+		registers_.PS[static_cast<uint8_t>(rps::Zero)] = registers_.A == 0 ? true : false;
+	}
+}
---
+++ NES_Core/src/nes/cpu/opcodes/accumulatorExecutor.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "nes/cpu/opcodes/basicExecutor.h"
+
+namespace nes::cpu::opcodes{
+	class AccumulatorExecutor : public BasicExecutor
+	{
+	public:
+		AccumulatorExecutor(nes::cpu::registers::Registers& registers, nes::memory::Memory& memory);
+		~AccumulatorExecutor() = default;
+		AccumulatorExecutor(const AccumulatorExecutor& rhs) = delete;
+		AccumulatorExecutor(AccumulatorExecutor&& rhs) = delete;
+		AccumulatorExecutor& operator=(const AccumulatorExecutor& rhs) = delete;
+		AccumulatorExecutor& operator=(AccumulatorExecutor&& rhs) = delete;
+
+		void ASL() noexcept;
+	private:
+	private:
+	};
+}
