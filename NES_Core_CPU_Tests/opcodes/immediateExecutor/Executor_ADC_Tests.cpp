075a08a556a279d5bc00c2548333fd1c03387960
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-09-18 18:49:52 +0200

    Refactored immediate ADC tests

--- NES_Core_CPU_Tests/opcodes/immediateExecutor/Executor_ADC_Tests.cpp
+++ NES_Core_CPU_Tests/opcodes/immediateExecutor/Executor_ADC_Tests.cpp
@@ -26,16 +26,20 @@ namespace OPCodes_ImmediateExecutor
 		{
 			int8_t val = 0x01;
 			int8_t previousA = reg_.A;
+			int8_t expectedResult = int8_t(previousA + val);
+			Assert::AreNotEqual(expectedResult, reg_.A);
 
 			ie_.ADC(val);
 
 			Assert::AreEqual(int8_t(previousA + val), reg_.A);
 		}
 
-		TEST_METHOD(ADC_adds_value_to_A_register_edge_case)
+		TEST_METHOD(ADC_adds_value_to_A_register_max_value)
 		{
 			int8_t val = 0xFF;
 			int8_t previousA = reg_.A;
+			int8_t expectedResult = int8_t(previousA + val);
+			Assert::AreNotEqual(expectedResult, reg_.A);
 
 			ie_.ADC(val);
 
